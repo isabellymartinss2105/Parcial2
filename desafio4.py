e079be845e5a0b977c33edc368d13b09a83bf232
isabellymartinss2105 <martins.santos.isabelly@escola.pr.gov.br>
2026-04-08 18:49:29 -0300

    Add basic calculator functionality in desafio4.py

---
+++ desafio4.py
@@ -0,0 +1,21 @@
+# CALCULADORA
+
+n1 = float(input("Selecione um número: "))
+n2 = float(input("Selecione outro número: "))
+
+operacao = input("Escolha uma operação (soma, subtração, multiplicação ou divisão): ")
+
+if operacao == "soma":
+    print(n1 + n2)
+
+elif operacao == "subtração":
+    print(n1 - n2)
+
+elif operacao == "multiplicação":
+    print(n1 * n2)
+
+elif operacao == "divisão":
+    print(n1 / n2)
+
+else:
+    print("Essa operação não existe")
