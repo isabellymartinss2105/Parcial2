5e9e859c7da35adbecfa09a8a2b8591f2b02a3f2
isabellymartinss2105 <martins.santos.isabelly@escola.pr.gov.br>
2026-04-08 19:38:36 -0300

    Fix print statement for displaying interest value

--- desafio7.py
+++ desafio7.py
@@ -4,4 +4,4 @@ capital = float(input("Digite seu capital (C): "))
 taxa = float(input("Digite a taxa de juros (I): "))
 tempo = float(input("Digite o tempo (T): "))
 juros = (capital * taxa * tempo) / 100
-print("O valor dos juros é", juros)
+print("O valor dos juros é", juros
